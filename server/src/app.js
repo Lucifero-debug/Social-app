67edf8ab0434b33ce668a1bb09c4772fefffd351
Lucifero-debug <shishupal78008@gmail.com>
2024-10-29 10:18:55 +0530

    jerk

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
   cors:{
-      origin:["https://social-app-frontend-alpha.vercel.app"],
+      origin:["https://localhost:5173"],
       methods:["GET","POST"]
   }
 }) 
